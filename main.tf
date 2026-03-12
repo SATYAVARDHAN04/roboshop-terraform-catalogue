2ff7cd7deb6b6c4001665f4d3a9d599aecada8cc
SATYAVARDHAN04 <svardhan278@gmail.com>
2026-03-12 15:52:02 +0530

    terraform

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_lb_target_group" "catalogue" {
   name     = "${var.project}-${var.environment}-catalogue"
   port     = 8080
   protocol = "HTTP"
-  vpc_id   = data.aws_ssm_parameter.vpc_id.id
+  vpc_id   = data.aws_ssm_parameter.vpc_id.value
   health_check {
     healthy_threshold   = 2
     interval            = 5
